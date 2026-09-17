bda69fe3bb91f8d6b7ac0573d41928e4d9458246
bogdanmaciuca <bogdanmaciuca0621@gmail.com>
2026-09-17 12:34:34 +0300

    storage buffer

--- src/engine.h
+++ src/engine.h
@@ -5,18 +5,19 @@ typedef struct Engine_t {
     SDL_GPUDevice*           device;
     SDL_GPUTextureFormat     swapchain_texture_format;
     SDL_GPUGraphicsPipeline* graphics_pipeline;
-    SDL_GPUComputePipeline*  compute_pipeline;
-    const char*              compute_shader_path;
-    SDL_Time                 compute_shader_modified_time;
     SDL_GPUTexture*          screen_texture;
     SDL_GPUSampler*          screen_texture_sampler;
 
+    SDL_GPUComputePipeline*   compute_pipeline;
+    const char*               compute_shader_path;
+    SDL_Time                  compute_shader_modified_time;
     shaderc_compiler_t        shader_compiler;
     shaderc_compile_options_t shader_compile_options;
 
+    SDL_GPUBuffer* storage_buffer;
+
     u32 swapchain_texture_width;
     u32 swapchain_texture_height;
-
     u32 screen_texture_width;
     u32 screen_texture_height;
 } Engine;
